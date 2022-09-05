4701156b7bf1ab85dcbbbb3beffcbba015b4b2e2
Devinsa19 <devianisa1219@students.unnes.ac.id>
2022-09-05 14:18:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('master.lead.index');
+Route::get('/', function () 
+{
+    return view('master.lead.index'); 
 });
