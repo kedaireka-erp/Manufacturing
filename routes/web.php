52ef3cca12adf444dea7a38cd24eee45889c15fe
agistarakha <agistarakhasnc@gmail.com>
2022-09-14 11:23:41 +0700

    bugfix

--- routes/web.php
+++ routes/web.php
@@ -23,35 +23,36 @@ use Illuminate\Support\Facades\Route;
 route::get("/show1", function () {
     return view("manufaktur.show");
 });
-route::get("/", function () {
-    return view("items.index");
-Route::get(function() {
-    echo "Hello";
-});
+// route::get("/", function () {
+//     return view("items.index");
+// });
+// Route::get(function () {
+//     echo "Hello";
+// });
 
 //Route leads
-Route::controller(LeadController::class)->group(function(){
-    Route::get('/leads','index')->name('leads');
-    Route::get('/lead/create','create')->name('createLead');
-    Route::post('/lead/store','store')->name('storeLead');
-    Route::get('/lead/edit/{id}','edit')->name('editLead');
-    Route::post('/lead/update/{id}','update')->name('updateLead');
-    Route::get('/lead/delete/{id}','destroy')->name('deleteLead');
-    Route::get('/lead/trash','trash');
-    Route::post('/lead/restore/{id}','restore');
-    Route::get('/lead/search','search');
+Route::controller(LeadController::class)->group(function () {
+    Route::get('/leads', 'index')->name('leads');
+    Route::get('/lead/create', 'create')->name('createLead');
+    Route::post('/lead/store', 'store')->name('storeLead');
+    Route::get('/lead/edit/{id}', 'edit')->name('editLead');
+    Route::post('/lead/update/{id}', 'update')->name('updateLead');
+    Route::get('/lead/delete/{id}', 'destroy')->name('deleteLead');
+    Route::get('/lead/trash', 'trash');
+    Route::post('/lead/restore/{id}', 'restore');
+    Route::get('/lead/search', 'search');
 });
 //Route Subkons
-Route::controller(SubkonController::class)->group(function(){
-    Route::get('/subkons','index')->name('subkons');
-    Route::get('/subkon/create','create')->name('createSubkon');
-    Route::post('/subkon/store','store')->name('storeSubkon');
-    Route::get('/subkon/edit/{id}','edit')->name('editSubkon');
-    Route::post('/subkon/update/{id}','update')->name('updateSubkon');
-    Route::get('/subkon/delete/{id}','destroy')->name('deleteSubkon');
-    Route::get('/subkon/trash','trash');
-    Route::get('/subkon/restore/{id}','restore');
-    Route::get('/subkon/search','search');
+Route::controller(SubkonController::class)->group(function () {
+    Route::get('/subkons', 'index')->name('subkons');
+    Route::get('/subkon/create', 'create')->name('createSubkon');
+    Route::post('/subkon/store', 'store')->name('storeSubkon');
+    Route::get('/subkon/edit/{id}', 'edit')->name('editSubkon');
+    Route::post('/subkon/update/{id}', 'update')->name('updateSubkon');
+    Route::get('/subkon/delete/{id}', 'destroy')->name('deleteSubkon');
+    Route::get('/subkon/trash', 'trash');
+    Route::get('/subkon/restore/{id}', 'restore');
+    Route::get('/subkon/search', 'search');
 });
 route::get("/", [ItemController::class, "index"]);
 route::get("/create", [ItemController::class, "create"])->name("create");
